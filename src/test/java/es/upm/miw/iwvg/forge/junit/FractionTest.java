4b7932a7919b5446dc784d3bd212ed17e5f0e9a5
DiegoRiofrio <vinicio1716@gmail.com>
2018-09-24 18:58:48 +0200

    creacion de Test

---
+++ src/test/java/es/upm/miw/iwvg/forge/junit/FractionTest.java
@@ -0,0 +1,39 @@
+package es.upm.miw.iwvg.forge.junit;
+
+
+import es.upm.miw.iwvg.forge.junit.Fraction;
+import org.apache.logging.log4j.LogManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class FractionTest {
+
+    private Fraction fraction;
+
+    @BeforeEach
+    void before(){
+        fraction = new Fraction(20,4);
+    }
+
+    @Test
+    void testNumeradorPorDefecto(){
+        fraction = new Fraction();
+        Assertions.assertEquals(1,fraction.getNumerator());
+    }
+
+    @Test
+    void testFractionNumerator() {
+        Assertions.assertEquals(20, fraction.getNumerator());
+    }
+    @Test
+    void testFractionDenominator() {
+        Assertions.assertEquals(4, fraction.getDenominator());
+    }
+
+    @Test
+    void testFraccionDecimal(){
+        Assertions.assertEquals( 5, fraction.decimal(), 10e-5);
+    }
+}
---
+++ src/test/java/es/upm/miw/iwvg/forge/junit/UserTest.java
@@ -0,0 +1,44 @@
+package es.upm.miw.iwvg.forge.junit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+    private User user;
+
+    @BeforeEach
+    void before() {
+
+        this.user = new User(10,"Diego", "Maradona");
+
+    }
+
+    @Test
+    void testInitials() {
+        Assertions.assertEquals("D.",user.initials());
+    }
+
+    @Test
+    void testUserFullName() {
+
+        Assertions.assertEquals("Diego Maradona", user.getName() + " " + user.getFamilyName());
+    }
+
+    @Test
+    void testUserNumber() {
+
+        Assertions.assertEquals(10, user.getNumber());
+    }
+
+    @Test
+    void testUserName() {
+        Assertions.assertEquals("Diego", user.getName());
+    }
+
+    @Test
+    void testUserFamilyName() {
+        Assertions.assertEquals("Maradona", user.getFamilyName());
+    }
+}
